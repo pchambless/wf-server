e8d8510bc0de9a45e427c98e66a1bd593243c365
Paul C <pc7900@gmail.com>
2025-01-14 08:10:37 -0600

    Begin Tracking SQL

--- server/sql/api_userAccts.sql
+++ server/sql/api_userAccts.sql
@@ -1,16 +1,16 @@
 CREATE OR REPLACE VIEW api_userAccts
 as
 SELECT	case when c.NAME IS NULL then '' ELSE c.NAME end  acctName
-,        a.first_name
-,			CONCAT(a.first_name, ' ', a.last_name) user_name
-,			case when a.role = 1 then 'Global' ELSE '' END role	
-,			case when b.is_owner then 'Owner' ELSE '' END owner
+,        a.first_name                         firstName
+,			CONCAT(a.first_name, ' ', a.last_name)  fullName
+,			case when a.role = 1 then 'Global' ELSE '' END roleID
+,			case when b.is_owner then 'Owner' ELSE '' END ownerFlag
 ,        a.email
-,			a.id												user_id
-,			b.account_id									acctID
-,        b.is_owner	
-,			cast(b.id as unsigned)						acct_user_id
-,			a.default_account_id							dflt_acct_id
+,			a.id												            userID
+,			b.account_id									          acctID
+,     b.is_owner	                            isOwner
+,			cast(b.id as unsigned)						      acctUserID
+,			a.default_account_id							      dfltAcctID
 FROM    		whatsfresh.users a
 JOIN			whatsfresh.accounts_users b
 ON				a.id = b.user_id
